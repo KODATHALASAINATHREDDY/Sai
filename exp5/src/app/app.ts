8a61329bfeaa2733377aea38c684c328d128a716
KODATHALASAINATHREDDY <229x1a05h7@gprec.ac.in>
2025-10-07 09:34:13 +0530

    Update app.ts

--- exp5/src/app/app.ts
+++ exp5/src/app/app.ts
@@ -1,29 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { Home } from './home/home';
-import { Recruiter } from './recruiter/recruiter';
-import { Applicant } from './applicant/applicant';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ Home, Recruiter, Applicant, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('exp3');
+  teachers = [
+    { name: 'Dr.N.Kasiviswanath', dep: 'CSE', gender: 'Male' },
+    { name: 'Smt.D.L.N.Prasunna', dep: 'CSE', gender: 'Female' },
+    { name: 'Smt.T.Chandana', dep: 'ECS', gender: 'Female' },
+    { name: 'Dr.R.Praveen Sam', dep: 'ECS', gender: 'Male' },
+    { name: 'Smt. Srilakshmi', dep: 'CSE', gender: 'Female' }
+  ];
 
+  selectedDep = '';
+  selectedGen = '';
 
-  currentView: string = 'home';
-
-  showApplicant() {
-    this.currentView = 'applicant';
-  }
-  showRecruiter() {
-    this.currentView = 'recruiter';
-  }
-  showHome() {
-    this.currentView = 'home';
-  }
+ get filteredFaculty() {
+  return this.teachers.filter(f =>(this.selectedDep ? f.dep === this.selectedDep : true) &&
+ (this.selectedGen ? f.gender === this.selectedGen : true) );
+ }
 }
