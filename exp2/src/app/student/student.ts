1098123e43b98c2587fc850dfc72603a43a03a2b
KODATHALASAINATHREDDY <1919sainathreddy@gmail.com>
2025-10-28 18:00:45 +0530

    Added new exp2 lab program

---
+++ exp2/src/app/student/student.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+@Component({
+selector: 'app-student',
+standalone: true,
+imports: [CommonModule, FormsModule],
+templateUrl: './student.html',
+styleUrls: ['./student.css']
+})
+export class Student {
+registerStudent() {
+alert('Student registration submitted!');
+}
+}
